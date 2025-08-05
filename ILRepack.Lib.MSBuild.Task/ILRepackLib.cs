d8757b66c60529efd01577c17c5723f2aee69106
Christian Helmich <kagekirin@gmail.com>
2025-08-05 15:16:13 +0900

    refactor: write Internalize Exclude assembly names with file extension if provided (refactor/simplify-internalize-exclude) [#73]

--- ILRepack.Lib.MSBuild.Task/ILRepackLib.cs
+++ ILRepack.Lib.MSBuild.Task/ILRepackLib.cs
@@ -535,13 +535,7 @@ public class ILRepack : Microsoft.Build.Utilities.Task, IDisposable
             );
 
             foreach (var asm in InternalizeExclude)
-            {
-                writer.WriteLine(
-                    asm.ItemSpec.EndsWith(".dll")
-                        ? Path.GetFileNameWithoutExtension(asm.ItemSpec)
-                        : Path.GetFileName(asm.ItemSpec)
-                );
-            }
+                writer.WriteLine(Path.GetFileName(asm.ItemSpec));
 
             repackOptions.ExcludeFile = excludeFile;
         }
--- ILRepack.Tool.MSBuild.Task/ILRepack.cs
+++ ILRepack.Tool.MSBuild.Task/ILRepack.cs
@@ -539,13 +539,7 @@ public class ILRepack : Microsoft.Build.Utilities.Task, IDisposable
             );
 
             foreach (var asm in InternalizeExclude)
-            {
-                writer.WriteLine(
-                    asm.ItemSpec.EndsWith(".dll")
-                        ? Path.GetFileNameWithoutExtension(asm.ItemSpec)
-                        : Path.GetFileName(asm.ItemSpec)
-                );
-            }
+                writer.WriteLine(Path.GetFileNameWithoutExtension(asm.ItemSpec));
 
             cmdParams.Add($"/internalize:\"{excludeFile}\"");
         }
